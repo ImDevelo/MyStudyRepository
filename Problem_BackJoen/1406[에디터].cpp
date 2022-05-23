75fc6be963a7dd4eaf884edf1a925bbca074a825
imdlsrks <imdlsrks.mc@gmail.com>
2022-05-23 17:09:38 +0900

    commit 23/05/22

--- Problem_BackJoen/1406[에디터].cpp
+++ Problem_BackJoen/1406[에디터].cpp
@@ -4,37 +4,153 @@
 #include<string>
 using namespace std;
 
-void MySolution(){
-    int N, M, cuser;
-    string s;
-    cin >> s >> M;
-    cuser = s.length();
-    for(int i=0; i<M;i++){
-        char c;
-        cin >> c;
-        if(c == 'L'){
-            if(cuser > 0)
-                cuser--;
-        }else if(c == 'D'){
-            if(cuser < s.length())
-                cuser++;
-        }else if(c == 'B'){
-            if(cuser > 0){
-                s.erase(cuser-1,1);
-                cuser--;
+class LinkedList{
+public:
+    LinkedList(){
+        first = nullptr;
+        last = nullptr;
+        cuser = 0;
+        lenght = 0;
+    }
+
+    void add(char c){
+        Node* p = new Node(c);
+        lenght++;
+        cuser++;
+        if(first == nullptr){
+            first = p;
+            last = p;
+        }else{
+            p->prv = last;
+            last->next = p;
+            last = p;
+        }
+    }
+
+    void insert(char c){
+        if(lenght > 0){
+            Node* node = new Node(c);
+            if(cuser == 0){
+                node->next = first;
+                first->prv = node;
+                first = node;
+            }else{
+                Node* p = first;
+                for(int i=1;i<cuser;i++){
+                    p = p->next;
+                }
+                node->next = p->next;
+                p->next = node;
+                node->prv = p;
             }
-        }else if(c == 'P'){
-            string temp;
-            cin >> temp;
-            if(cuser < s.length()){
-                s.insert(cuser,temp);
-            }else if(cuser >= s.length()){
-                s.append(temp);
+            cuser++; lenght++;
+        }
+        
+    }
+
+    void Delete(){
+        if(cuser > 0){
+            if(cuser == 1){
+                Node* temp = first;
+                first = first->next;
+                first->prv = nullptr;
+                delete temp;
+            }else{
+                Node* p = first;
+                for(int i=1; i<cuser;i++){
+                    p = p->next;
+                }
+                Node* temp = p;
+                Node* privous = p->prv;
+                if(p->next == nullptr){
+                    privous->next = nullptr;
+                }else{
+                    privous->next = p->next;
+                    p->next->prv = privous;
+                }
+                delete temp;
             }
+            cuser--;
+            lenght--;
+        }
+    }
+
+    void Left(){
+        if(cuser > 0){
+            cuser--;
+        }
+    }
+
+    void Right(){
+        if(cuser < lenght){
             cuser++;
         }
     }
-    cout << s;
+
+    void print(){
+        Node* p = first;
+        for(int i=0; p != nullptr ;i++){
+            cout << p->data;
+            p = p->next;
+        }
+
+    }
+
+private:
+    class Node{
+        Node(char c){
+            prv = nullptr;
+            next = nullptr;
+            data = c;
+        }
+        char data;
+        Node* prv;
+        Node* next;
+        friend LinkedList;
+    };
+    Node* first;
+    Node* last;
+    int lenght;
+    int cuser;
+};
+
+
+void MySolution(){
+    LinkedList l;
+    string s;
+    int N;
+    char c;
+    cin >> s;
+    for(int i=0; i<s.length();i++){
+        l.add(s.at(i));
+    }
+
+    cin >> N;
+    for(int i=0; i<N;i++){
+        cin >> c;
+        switch (c)
+        {
+        case 'L':
+            l.Left();
+            break;
+        case 'D':
+            l.Right();
+            break;
+        case 'B':
+            l.Delete();
+            break;
+        case 'P':
+            cin >> c;
+            l.insert(c);
+            break;
+        default:
+            break;
+        }
+    }
+    
+
+    l.print();
+
 }
 
 int main(){
@@ -42,5 +158,4 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
     MySolution();
-
 }
