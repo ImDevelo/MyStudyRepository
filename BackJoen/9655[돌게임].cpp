decc96dd1d873a6f46e8bf6556e9573a99fee47b
SH Woo <imdlsrks.mc@gmail.com>
2023-07-12 22:40:36 +0900

    23/07/12

--- BackJoen/9655[돌게임].cpp
+++ BackJoen/9655[돌게임].cpp
@@ -1,11 +1,18 @@
-//
-//https://www.acmicpc.net/problem/
+//돌게임
+//https://www.acmicpc.net/problem/9655
 #include <iostream>
 using namespace std;
 #define MAX 0
 
 void MySolution(){
+    int N;
+    cin >> N;
 
+    if(N%2){
+        cout << "SK";
+    }else{
+        cout << "CY";
+    }
 }
 
 int main(){
