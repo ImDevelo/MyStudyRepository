8178bf440d2d040dc4fc310a6e91e39c18d12aee
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-03-03 15:35:52 +0900

    commit

--- Algorithms_VS/main.cpp
+++ Algorithms_VS/main.cpp
@@ -1,5 +1,34 @@
 #include<iostream>
-#include"2110.h"
+#include"10816_2.h"
+
+
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+
+void MySolution() {
+	int M, N;
+	multiset<int> number;
+	vector<int> search;
+
+
+	cin >> N;
+	for (int i = 0, temp; i < N; i++) {
+		cin >> temp;
+		number.insert(temp);
+	}
+
+	cin >> M;
+	for (int j = 0, temp; j < M; j++) {
+		cin >> temp;
+		search.push_back(temp);
+		cout << number.count(temp) << " ";
+	}
+}
+
+
 
 
 int main() {
