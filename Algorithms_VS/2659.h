be58c589fca8f699a30fe51435d0407e2992ef98
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-03-05 13:18:17 +0900

    commit

---
+++ Algorithms_VS/2659.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void MySolution() {
+	int number_index[4];
+
+	for (int i = 0; i < 4; i++)
+	{
+		cin >> number_index[i];
+	}
+
+	int cross_number = 9999;
+
+	for (int i = 0; i < 4; i++)
+	{
+		int number = 1000 * number_index[i % 4] + 100 * number_index[(i+1) % 4] +  10 * number_index[(i + 2) % 4] + number_index[(i + 3) % 4];
+
+		cross_number = min(number, cross_number);
+	}
+	//cout << cross_number << endl;
+	int count = 0;
+	bool find = false;
+	for (int i = 1; i < 10 && !find; i++)
+	{
+		for (int j = 1; j < 10 && !find; j++)
+		{
+			if (i >  j) {
+				continue;
+			}
+			for (int k = 1; k < 10 && !find; k++)
+			{
+				if (i > k) {
+					continue;
+				}
+
+				for (int l = 1; l < 10 && !find; l++)
+				{
+					if (i > l) {
+						continue;
+					}
+					int number = 1000 * i + 100 * j + 10 * k + l;
+					
+					
+					if (i == l && ( l < k || l < j )) {
+						//cout << "==========" << number << endl;
+						continue;
+					}
+					else if (i == k && (l < j)) {
+						//cout << "==========" << number << endl;
+						continue;
+					}
+					//cout << count << " " << number << endl;
+					count++;
+					if (number >= cross_number) {
+						find = true;
+						break;
+					}
+				}
+			}
+		}
+	}
+	cout << count;
+}
