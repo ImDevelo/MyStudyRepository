a985240a9402db32c91f91c57ac8d1470bd174f4
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-03-30 21:38:36 +0900

    YOLOtest

---
+++ openCV/YOLO/pytouch.py
@@ -0,0 +1,7 @@
+import torch
+import torchvision
+import torchaudio
+
+print("PyTorch version:", torch.__version__)
+print("torchvision version:", torchvision.__version__)
+print("torchaudio version:", torchaudio.__version__)
