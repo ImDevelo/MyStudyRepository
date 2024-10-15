3739eaa01dbbfca3c5a634347c2d76337d3c5dc6
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-10-15 20:51:09 +0900

    숫자고르기

--- code test/BackJoen/2668.숫자고르기.cpp
+++ code test/BackJoen/2668.숫자고르기.cpp
@@ -1,58 +1,69 @@
-//ìˆ«ì ê³ ë¥´ê¸°
 //https://www.acmicpc.net/problem/2668
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
 using namespace std;
-#define MAX 100
 
-void MySolution(){
-    int N;
-    int arr[MAX];
-    int count = 0;
-    cin >> N;
-    
-    for(int i=0; i<N;i++){
-        int temp;
-        cin >> temp;
-        arr[i] = temp - 1;
-    }
-
-    int check[MAX];
-
-    for(int i=0; i<N;i++){
-        if(check[i] != true){
-            int p = arr[i];
-            for(int j =0; j<N ; j++){
-                if(arr[p] == i){
-                    check[i] = true;
-                    count++;
-                    break;
-                }
-                p = arr[p]; 
-            }
-
-            if(check[i] == true){
-                int x = arr[i];
-                while (x != i)
-                {
-                    count++;
-
-                    check[x] = true;
-                    x = arr[x];
-                }
-            }
-        }
-    }
-    cout << count << endl;
-    for(int i =0; i<N;i++){
-        if(check[i] == true){
-            cout << i + 1 << endl;
-        }
-    }
-}
-
-int main(){
+int main() {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    MySolution();
+    cin.tie(NULL); cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    vector<int> arr(N + 1);
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+
+    vector<bool> term(N + 1, false), visited(N + 1, false);
+    vector<int> ans;
+    for (int i = 1; i <= N; i++) {
+        if (visited[i] == true) {
+            continue;
+        }
+        // DFS ÅÒ ±¸ÇÏ±â
+        vector<int> pos;
+        int index = i, cir_num = 0;
+        pos.push_back(index);
+
+        while (true)
+        {
+            int next_index = arr[index];
+            visited[index] = true;
+            if (term[next_index] == true) {
+                // ´ÙÀ½ ¼ıÀÚ°¡ ÅÒÀÎ °æ¿ì
+                break;
+            }
+            else if (visited[next_index] == true) {
+                // ¼øÈ¯µÇ´Â ¹øÈ£¸¦ Ã£Àº °æ¿ì
+                cir_num = next_index;
+                break;
+            }
+            else {
+                pos.push_back(next_index);
+                index = next_index;
+            }
+        }
+
+        bool isTerm = false;
+        for (int p : pos) {
+            
+            if (p == cir_num) {
+                isTerm = true;
+            }
+            term[p] = isTerm;
+            if (term[p]) {
+                ans.push_back(p);
+            }
+        }
+
+    }
+
+    cout << ans.size() << endl;
+    sort(ans.begin(), ans.end());
+    for (int n : ans) {
+        cout << n << " ";
+    }
+
 }
